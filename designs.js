678c195ff4026bffba9b64ea6edf1e75b4400c82
Wojciech Kogucki <wkogucki@gmail.com>
2018-08-01 17:00:54 +0200

    Creating the size pick function

--- designs.js
+++ designs.js
@@ -1,10 +1,20 @@
 // Select color input
 // Select size input
-
+let height, width, color;
 // When size is submitted by the user, call makeGrid()
+
+
+const submit = document.getElementById("sizePicker");
+submit.addEventListener('submit', function(event) {
+  event.preventDefault();
+  height = document.getElementById("inputHeight").value;
+  width = document.getElementById("inputWidth").value;
+  makeGrid(height, width)
+  console.log('Height: ' + height + ' and width: ' + width);
+});
+
 
 function makeGrid() {
 
-// Your code goes here!
 
 }
